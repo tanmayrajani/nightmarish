69e8c58b6f7655a7aaac7b01f9287f27ced639bb
Tanmay Rajani <rajani.tanmay@gmail.com>
2017-02-26 13:50:46 +0530

    renames index to google-search

---
+++ google-search.js
@@ -0,0 +1,31 @@
+var Nightmare = require('nightmare');       
+var nightmare = Nightmare({
+  /*openDevTools: {
+    mode: 'detach'
+  },*/
+  show: true
+});
+
+nightmare
+  .goto('http://www.footlocker.com/product/model:236830/sku:S80686/adidas-ultra-boost-womens/')
+  .type('#lst-ib', process.argv[2] ? process.argv[2] : 'tanmay rajani')
+  // .type('body', '\u000d')
+  .click('.sbico-c#_fZl')
+  .wait(3000)
+  .evaluate(function () {
+    var nodes = document.querySelectorAll('.g .rc a:not([id^=am]):not(.fl):not(.gl)');
+    var links = [];
+    nodes.forEach(function (node) {
+      links.push(node.getAttribute('href'))
+    })
+    return links;
+  })
+  .end()
+  .then(function (result) {
+    result.forEach(function(link) {
+      console.log(link)
+    })
+  })
+  .catch(function (error) {
+    console.error('Search failed:', error);
+  });
